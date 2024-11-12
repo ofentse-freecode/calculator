a62b421ae1eba56e2e6332f090cc7ca67cfd735d
ofentse-freecode <ofentse.allphotos@gmail.com>
2024-11-12 17:14:35 +0200

    feat:buttons

---
+++ index.js
@@ -0,0 +1,67 @@
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.getElementsByClassName('.operation');
+const equalButton = document.getElementsByClassName('.btn-eql');
+const deleteButton = document.getElementsByClassName('.del-btn');
+const allClear = document.getElementById("all-clear");
+let currentText = document.getElementsByClassName(".current");
+const previousText = document.getElementsByClassName('.previous');
+
+
+//currentText.textContent = "";
+//previousText.innerHTML = "3";
+let calculation = [];
+
+
+//functions
+function clear(){
+
+}
+
+function deleteNumber(){
+
+}
+
+function appendNumber(value){
+//currentText.textContent = number.toString() + number.toString()
+calculation.push(value);
+fullnumber = calculation.join("")
+previousText.innerHTML = fullnumber;
+}
+
+function chooseOperation(){
+
+}
+
+function compute(){
+
+}
+
+function updateDisplay(){
+    currentText.innerText = number;
+    //currentText.appendChild(number)
+}
+
+
+numberButtons.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+       const value = button.innerText;
+        appendNumber(value);
+        //updateDisplay();
+       console.log(previousText.innerHTML)
+       
+    })
+});
+allClear.addEventListener('click', function(){
+    console.log('clicked');
+   
+})
+
+//button.addEventListener('click', function(){
+    //show.classList.toggle("hide-share");
+    //console.log("clicked");
+    //blogCard.appendChild(title)
+//});
+
+
+
+
