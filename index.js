f9777469e8cf582261d5d8201113cc0453185819
Priviledge <privilledgeblack8@gmail.com>
2024-11-13 08:21:55 +0200

    functions..updateDisplay(), clear(), deleteNumber() are complete

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const numberButtons = document.querySelectorAll('.number');
 const operationButtons = document.getElementsByClassName('.operation');
 const equalButton = document.getElementsByClassName('.btn-eql');
-const deleteButton = document.getElementsByClassName('.del-btn');
+const deleteButton = document.querySelector('.del-btn');
 const allClear = document.getElementById("all-clear");
 let currentText = document.getElementsByClassName(".current");
 const previousText = document.getElementsByClassName('.previous');
 //testing bellow
 const displayinput = document.querySelector("#display")
 console.log(displayinput)
+console.log(deleteButton)
 
 //currentText.textContent = "";
 //previousText.innerHTML = "3";
@@ -15,18 +16,37 @@ let calculation = [];
 
 
 //functions
-allClear.addEventListener('click', function(){
-    displayinput.value = ""
-    console.log('clicked');
-   
-})
-function clear(e){
+//function for updating the input field
+function updateDisplay(){
+    // currentText.innerText = number;
+    // currentText.appendChild(number)
+    document.addEventListener("click", (event)=>{
+     if(event.target.matches(".number, .operation")){
+   const value = event.target.getAttribute("data-value");
+   displayinput.value += value;
+   console.log(displayinput.value);
+     }
+ })
+ }
 
+
+
+//function to remove all from input
+function clear(){
+    allClear.addEventListener('click', function(){
+        displayinput.value = ""
+        console.log('clicked');
+       
+    })
 }
-
+//function to delete a single degit
 function deleteNumber(){
-
+    deleteButton.addEventListener("click", ()=>{
+        displayinput.value = displayinput.value.slice(0, -1);
+        console.log(displayinput.value)
+       })
 }
+
 
 function appendNumber(value){
 //currentText.textContent = number.toString() + number.toString()
@@ -43,11 +63,10 @@ function compute(){
 
 }
 
-function updateDisplay(){
-    currentText.innerText = number;
-   // currentText.appendChild(number)
-}
-
+//calling all functions
+updateDisplay()
+clear()
+deleteNumber()
 
 /*numberButtons.forEach((button)=>{
     button.addEventListener("click", ()=>{
@@ -58,13 +77,7 @@ function updateDisplay(){
        
     })
 });*/
-document.addEventListener("click", (event)=>{
-    if(event.target.matches(".number, .operation")){
-  const value = event.target.getAttribute("data-value");
-  displayinput.value += value;
-  console.log(displayinput.value);
-    }
-})
+
 
 
 
